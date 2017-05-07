5e6a4f2f3a1d2470ea23fc76dbf4ac492d7abad9
mjuar005 <mjuar005@fiu.edu>
2017-05-07 18:56:14 -0400

    Final Geoquiz update
    - Added animation to cheat button

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -5,14 +5,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import static android.R.attr.start;
-import static android.widget.Toast.makeText;
 
 public class QuizActivity extends AppCompatActivity {
 
